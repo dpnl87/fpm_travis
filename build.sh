58fa26099d29b8e475da4d152641a6785653a673
Daniel Paulus <d.paulus@gmail.com>
2014-06-22 21:28:34 +0200

    alpha build scrupt

--- build.sh
+++ build.sh
@@ -1,1 +1,52 @@
-echo "Running build"
+#!/bin/sh
+
+basedir=`pwd`
+
+# Create required directories
+mkdir -p $basedir/build_root_centos/usr/local
+mkdir -p $basedir/build_root_centos/etc/rc.d/init.d
+mkdir -p $basedir/build_root_centos/etc/sysconfig
+
+# Fetch the latest code, build the libraries and
+# remove files we don't need
+cd $basedir/build_root_centos/usr/local
+git clone https://github.com/rashidkpc/Kibana.git ./kibana
+cd kibana
+bundle install --deployment
+rm -rf .git .travis.yml .gitignore
+
+# Copy the init file and make it executable
+#cd $basedir/build_root_centos/etc/rc.d/init.d
+#cp $basedir/kibana.init.centos ./kibana
+#chmod a+rx kibana
+
+# Copy the default file
+#cd $basedir/build_root_centos/etc/sysconfig
+#cp $basedir/kibana.sysconfig.centos ./kibana
+
+# Ensure everythign has the correct settings
+cd $basedir/build_root_centos
+find . -type d -exec chmod a+rx {} \;
+find . -type f -exec chmod a+r {} \;
+
+# Lets build the package
+cd $basedir
+fpm -s dir \
+-t rpm \
+-n kibana \
+-v 0.2.0 \
+--iteration '1.el6' \
+--license MIT \
+--category 'System Environment/Daemons' \
+-a 'noarch' \
+--description 'Kibana is a web interface for Logstash.' \
+--url 'http://kibana.org' \
+--vendor 'Kibana.Org' \
+--maintainer "Daniel Paulus" \
+-d 'ruby' \
+--rpm-user 'root' \
+--rpm-group 'root' \
+-C build_root_centos .
+
+# Clean up the build directory
+rm -rf $basedir/build_root_centos
